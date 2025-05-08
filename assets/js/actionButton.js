53bc51dd29f31615d6cc5f6a8150e481e476196e
João Cezar <joaovictor2010lima@gmail.com>
2025-05-08 00:55:24 -0300

    Update

--- assets/js/actionButton.js
+++ assets/js/actionButton.js
@@ -26,6 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }, 500);
         }
     }
+    
 
     //Adiciona o evento de scroll para exibir ou esconder os botões
     document.addEventListener('scroll', toggleButtons);
@@ -38,4 +39,20 @@ document.addEventListener('DOMContentLoaded', function() {
     whatsappButton.addEventListener('click', function() {
         window.location.href = 'https://wa.me/5521991162419';
     });
+
+    function scrollToContact() {
+        const contactSection = document.getElementById('contato');
+        contactSection.scrollIntoView({ behavior: 'smooth' });
+    }
+    //Ação ao clicar no botão "Contato"
+    function sendEmail() {
+        emailjs.sendForm('service_tpaecom', 'template_esllu1g', '#contactForm', '8PWfXOJuiCtmCMlKU')
+            .then((response) => {
+                alert('Mensagem enviada com sucesso!');
+            }, (error) => {
+                alert('Erro ao enviar mensagem. Tente novamente.');
+            });
+    }
+
+
 });
